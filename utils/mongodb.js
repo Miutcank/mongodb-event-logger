a0f0b0196ac2e032bc001046a013328c54f6b276
Zsolt <zsolt@miutcank.hu>
2015-07-02 19:21:52 +0200

    fix: wait for connection when methods are called before MongoDB connection is established

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -18,14 +18,26 @@ var mongoDbUtil = module.exports = function MongoDBConstructor(config) {
       log.error('Error in MongoDB connection', {error: error});
     });
 
+  mongoDbUtil.getConnection = function getConnection() {
+    if (mongoDbUtil.connection) {
+      return Promise.resolve(mongoDbUtil.connection);
+    } else {
+      return Promise.delay(200).then(mongoDbUtil.getConnection);  // recursive wait
+    }
+  };
+
   mongoDbUtil.find = function find(query) {
-    // TODO: check connection
-    return mongoDbUtil.connection.collection(config.collection).find(query).toArrayAsync();
+    return mongoDbUtil.getConnection()
+      .then(function gotConnection(connection) {
+        return connection.collection(config.collection).find(query).toArrayAsync();
+      });
   };
 
   mongoDbUtil.insert = function insert(data) {
-    // TODO: check connection
-    return mongoDbUtil.connection.collection(config.collection).insert(data);
+    return mongoDbUtil.getConnection()
+      .then(function gotConnection(connection) {
+        return connection.collection(config.collection).insert(data);
+      });
   };
 
   return mongoDbUtil;
